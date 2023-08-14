d49ae4b6af4256bf9ebb54d060a6308f46e2c7fe
yusuf <yusufkaankotanli@outlook.com>
2023-08-14 16:58:16 +0300

    first commit

---
+++ server/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"context"
+	"net"
+
+	"github.com/ykotanli/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+type server struct {
+	proto.UnimplementedAddServiceServer // Embed the UnimplementedAddServiceServer struct to implement the interface)
+}
+
+// Implement the methods from the AddServiceServer interface
+func (*server) Add(ctx context.Context, request *proto.Request) (*proto.Response, error) {
+	a, b := request.GetA(), request.GetB()
+	result := a + b
+	return &proto.Response{Result: result}, nil
+
+}
+
+func (*server) Multiply(ctx context.Context, request *proto.Request) (*proto.Response, error) {
+	a, b := request.GetA(), request.GetB()
+	result := a * b
+	return &proto.Response{Result: result}, nil
+}
+
+func main() {
+	listener, err := net.Listen("tcp", ":4040")
+	if err != nil {
+		panic(err)
+	}
+
+	srv := grpc.NewServer()
+	proto.RegisterAddServiceServer(srv, &server{}) // Register the server
+	reflection.Register(srv)
+
+	if e := srv.Serve(listener); e != nil {
+		panic(err)
+	}
+}
